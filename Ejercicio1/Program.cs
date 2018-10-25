da5dc776b4d101483d4fd22b24bf1cdb14c42bb7
Javier Torres Rojas <j.torres.rojas@outlook.cl>
2018-10-25 19:04:59 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el tamaño del arreglo:");
+            int largo = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[largo];
+            int[] arrayResultado = new int[largo];
+            int num = 1;
+            for (int i = 0; i < largo; i++)
+            {
+                array[i] = num;
+                num++;
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine("");
+
+            Console.WriteLine("Ingrese número de rotaciones:");
+            int rotar = Convert.ToInt32(Console.ReadLine());
+
+            for (int u = 0; u < largo; u++)
+            {
+                int index = ((u + rotar) % largo);
+
+                arrayResultado[u] = array[index];
+
+                Console.Write(arrayResultado[u] + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
